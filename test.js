d596f85af771eb5c13a8c45fd8d1c2eb84bd93bd
joe gendreau <jgendreau77@gmail.com>
2020-02-05 13:39:54 -0500

    removed alert for test.js and added if statement

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
 // JavaScript Document
 
-alert("Hello, World!");
+var youLikeMeat = true;
+if (youLikeMeat){
+	
+	document.write("you like meat");
+	
+} else{
+	
+	document.write("you hate meat");
+}
+
