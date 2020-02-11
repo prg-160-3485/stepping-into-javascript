dd2dd9dde3b2502af78a613e299137a635fd0182
joe gendreau <jgendreau77@gmail.com>
2020-02-11 11:40:03 -0500

    removed previous code in test.js and added function toreturn the average of 5 numbers

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
 // JavaScript Document
 
-var links = document.getElementsByTagName("a");
-
-for(i = 0; i < links.length; i++){
+function getAverage (a,b,c,d,e){
 	
-	links[i].className = "link-" + i;
-	
-	
+	var average = (a + b + c + d + e) /3;
+	console.log(average);
+	return average;
 	
 }
+
+var myResult = getAverage(2,4,6,8,10);
+console.log("the average is " + myResult);
