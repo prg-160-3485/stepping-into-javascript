1c4c47edea0b7534da0580f095b1a45f29156600
joe gendreau <jgendreau77@gmail.com>
2020-02-11 12:37:46 -0500

    moved console.log(the average is  + myResult +  inside the function code below function logResult Global is declared outside of the function. Local variable is defined inside of the function

--- test.js
+++ test.js
@@ -2,11 +2,18 @@
 
 function getAverage (a,b,c,d,e){
 	
-	var average = (a + b + c + d + e) /3;
+	var average = (a + b + c + d + e) /3; //local variable
 	console.log(average);
 	return average;
 	
 }
 
-var myResult = getAverage(2,4,6,8,10);
-console.log("the average is " + myResult);
+var myResult = getAverage(2,4,6,8,10); //global variable
+
+function logResult(){
+
+	console.log("the average is " + myResult + " inside the function");
+
+}
+
+logResult();
